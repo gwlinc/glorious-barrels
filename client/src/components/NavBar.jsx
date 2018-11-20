b1c9fdefb4c57b0dc276ca803f0b49b250bd5ebc
i-mckinney <baseballrocks@gmail.com>
2018-11-20 13:17:08 -0800

    updated navbar and linecard

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function NavBar(props) {
   return (
     <div className="navBar">
-      <button type="button" className="home" onClick={e => props.handlePage(e)}>Home</button>
-      <button type="button" className="about" onClick={e => props.handlePage(e)}>About Us</button>
-      <button type="button" className="line" onClick={e => props.handlePage(e)}>Line Card</button>
-      <button type="button" className="contact" onClick={e => props.handlePage(e)}>Contact Us</button>
-      <button type="button" className="sales" onClick={e => props.handlePage(e)}>Current Sales</button>
-      <button type="button" className="store" onClick={e => props.handlePage(e)}>Store</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>Home</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>About Us</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>Line Card</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>Contact Us</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>Current Sales</button>
+      <button type="button" className="NavButton" onClick={e => props.handlePage(e)}>Store</button>
     </div>
     // <ul className="navBar">
     //   <li className="home button" onClick={e => props.handlePage(e)}>Home</li>
