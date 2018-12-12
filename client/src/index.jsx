ec17fb99988334ac80c238505260c0c174c1da58
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-12-11 20:28:21 -0800

    removed bugsnag

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,6 @@ import App from './components/App';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
 
-
 if (typeof window !== 'undefined') {
   ReactDOM.render(<App />, document.getElementById('app'));
 }
