9690633387eb39f1663f8eca1a0982847c0b7d4d
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-12-11 20:30:59 -0800

    added conditional styling for home page based on menu

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import Carousel from './Carousel';
 
-function Home() {
+function Home(props) {
+  let style;
+  if (!props.menu) {
+    style = { paddingTop: 300 };
+  }
   return (
-    <div>
-      <div className="mainImg">
-        <Carousel />
-      </div>
-      <div className="smallbox">
-        <div className="articles">
-          <div className="article">
-            <img src="gwlinc+prolong.jpg" alt="Cutting" />
-            <h1>
-              About Us
-            </h1>
-            <h2>
-              Descriptions of GoldenWest
-            </h2>
-          </div>
-          <div className="article">
-            <img src="gwlinc1.jpg" alt="Cutting" />
-            <h1>
-              Titles that will blow your mind
-            </h1>
-            <h2>
-              Descriptions of the current stuff
-            </h2>
-          </div>
-          <div className="article">
-            <img src="gwlinc1.jpg" alt="Cutting" />
-            <h1>
-              Titles that will blow your mind
-            </h1>
-            <h2>
-              Descriptions of the current stuff
-            </h2>
-          </div>
+    <div style={style}>
+      <Carousel />
+      <div className="articles">
+        <div className="article">
+          <img src="gwlinc+prolong.jpg" alt="Cutting" />
+          <h1>
+            About Us
+          </h1>
+          <h2>
+            Descriptions of GoldenWest
+          </h2>
+        </div>
+        <div className="article">
+          <img src="gwlinc1.jpg" alt="Cutting" />
+          <h1>
+            Titles that will blow your mind
+          </h1>
+          <h2>
+            Descriptions of the current stuff
+          </h2>
+        </div>
+        <div className="article">
+          <img src="gwlinc1.jpg" alt="Cutting" />
+          <h1>
+            Titles that will blow your mind
+          </h1>
+          <h2>
+            Descriptions of the current stuff
+          </h2>
         </div>
       </div>
     </div>
